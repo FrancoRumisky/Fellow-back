25eb30e8561b15cb3bc24d4443fc9f49d2e0fe73
FrancoDatamatrix <franco.r@datamatrixcompany.com>
2025-02-17 19:16:02 -0300

    fix rounte event

--- routes/web.php
+++ routes/web.php
@@ -188,7 +188,7 @@ Route::post('deleteStoryFromAdmin', [PostController::class, 'deleteStoryFromAdmi
 /*|--------------------------------------------------------------------------|
 | Event Route 
 |--------------------------------------------------------------------------|*/
-Route::get('event', [EventController::class, 'event'])->middleware(['checkLogin'])->name('eventsList');
+Route::get('events', [EventController::class, 'events'])->middleware(['checkLogin'])->name('events');
 Route::post('eventsList', [PostController::class, 'eventsList'])->middleware(['checkLogin'])->name('eventsList');
 
 Route::post('deleteUserFromAdmin', [UsersController::class, 'deleteUserFromAdmin'])->middleware(['checkLogin'])->name('deleteUserFromAdmin');
